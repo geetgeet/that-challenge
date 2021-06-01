fbb7a8d0eb9acf58d18cb0024399a41a2979eaaa
geetgeet <gmoses728@gmail.com>
2021-06-01 16:10:08 +0200

    search??

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,21 @@
 /* -- FETCH FUNCRIONS--*/
 let products = [];
+let catagoty=[]
  async function getPosts() {
   // Get element to change
   let list = document.getElementById("li-items");
 
   // Fetch the data
-  await fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=60fcd15748ca4894965746c5aed9d9ef")
+  await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=60fcd15748ca4894965746c5aed9d9ef")
     .then((response) => response.json())
     .then((json) => {
       console.log(json.articles);
       json.articles.forEach(item => {
         products.push(item);
        createsaleItem(item);
+      
       });
-     
+      
     });
 }
 
@@ -21,9 +23,10 @@ function createsaleItem(article) {
   const listitem = `<li>
   <div class="product-card">
   <h2>${article.title}</h2>
+  <p> By:${article.author}</p>
       <h5>${article.description}</h5>
       <img src="${article.urlToImage}" class="fakeimg" style="height:200px;"/>
-      <p>${article.content}</p>
+      <p >${article.content}</p>
     </div>
  </li>`;
   let list = document.getElementById("li-items");
--- scripts/searchItem.js
+++ scripts/searchItem.js
@@ -1,31 +1,38 @@
-import API_KEY from "../.env"
-function searchProducts() {
+
+
+async function searchProducts() {
   // Get data from form
   let form = document.getElementById("search-form");
   let searchTerm = form.getElementsByTagName("input")[0].value;
+  // let searchTerm = form.getElementsByTagName("input")[0].value.replace(/ /g,'-');
+  let searchTermKey = form.getElementsByTagName("input")[1].value;
   // Create Regular Expression
   let searchRegEx = new RegExp(searchTerm, "i");
-  const key = process.env.API_KEY
+  let searchRegEx2 = new RegExp(searchTermKey, "i");
+
+
   // Fetch data to search from
-  fetch(`https://newsapi.org/v2/sources?apiKey=${API_KEY}`)
-    .then((response) => response.json())
-    .then((data) => {
-      // Filter data
-      let result = json.filter((sources) => {
-        return sources.category.search(searchRegEx) !== -1;
-      });
-      // Check if there are found results
-      if (result.length > 0) {
-        // Render items that matched
-        document.getElementById("li-items").innerHTML = "";
-        result.forEach((product) => createsaleItem(product));
-      } else {
-        // Alert nothing found
-        alert("nothing found");
-      }
-    })
-    .catch((err) => {
-      // Log any error from back end
-      console.log(err);
+  await fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=60fcd15748ca4894965746c5aed9d9ef`)
+  .then((response) => response.json())
+  .then((json) => {
+    // Filter data
+    let result = json.articles.filter((article) => {
+      return article.description.search(searchRegEx) !== -1 & article.title.search(searchRegEx) !== -1 
     });
+    // Check if there are found results
+    if (result.length > 0) {
+      // Render items that matched
+      document.getElementById("li-items").innerHTML = "";
+      result.forEach((product) => createsaleItem(product));
+    } 
+    
+    else {
+      // Alert nothing found
+      alert("nothing found");
+    }
+  })
+  .catch((err) => {
+    // Log any error from back end
+    console.log(err);
+  });
 }
